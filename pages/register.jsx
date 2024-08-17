dd3b21c561eb4c12b469dcf35a0738d2c184db55
Vivek20510 <Vivekbadodiya20510@acropolis.in>
2024-08-17 14:52:32 +0530

    Update register.jsx

--- pages/register.jsx
+++ pages/register.jsx
@@ -28,7 +28,7 @@ export default function Register() {
                     <div className="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-indigo-800 dark:border-indigo-700">
                         <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
                             <h1 className="text-xl font-bold leading-tight tracking-tight text-indigo-700 md:text-2xl dark:text-white">
-                                Create an Account
+                                Sign up
                             </h1>
                             <form onSubmit={handleSubmit} className=" space-y-4 md:space-y-6" action="#">
                                 <div className='text-left'>
@@ -46,7 +46,7 @@ export default function Register() {
 
                                 <button type="submit" className="w-full text-white bg-indigo-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">Sign Up</button>
                                 <p className="text-sm font-light text-indigo-500 dark:text-indigo-400">
-                                    Already have an account  <Link href="/" className="font-medium text-primary-600 hover:underline dark:text-primary-500">Sign In</Link>
+                                    Already have an account  <Link href="/" className="font-medium text-primary-600 hover:underline dark:text-primary-500">Login</Link>
                                 </p>
                             </form>
                         </div>
